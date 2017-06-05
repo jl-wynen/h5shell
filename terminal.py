c3b8ec721ccc6e0c3664a49673a79889d33efca2
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2017-06-05 12:28:45 +0200

    Docs for terminals

--- terminal.py
+++ terminal.py
@@ -1,6 +1,16 @@
 class Terminal:
+    """
+    Fallback terminal manager.
+    Does not support autocompletion and navigating the history.
+    Suppresses colours in output.
+    """
     
     class History:
+        """
+        Manages the history of entered commands.
+        The list can be accessed through History.history.
+        """
+        
         def __init__(self, maxLength=500):
             self._history = []
             self._maxLength = maxLength
@@ -8,10 +18,18 @@ class Terminal:
             self._current = ""
 
         def __nonzero__(self):
+            """True if the history is non-empty."""
             return len(self.history) != 0
 
-        # current ignored when not at end of history
         def back(self, current):
+            """
+            Move back one element in the history.
+            Raises an IndexError if already at the beginning of the history.
+            Arguments:
+            current -- String currently visible in terminal. Is ignored if not at end of history.
+            Returns:
+            Item in the history after moving back one step.
+            """
             if self._histPtr == 0:
                 raise IndexError("Tried to go beyond start of history.")
             if self._histPtr == len(self._history):
@@ -20,6 +38,12 @@ class Terminal:
             return self._history[self._histPtr]
 
         def forward(self):
+            """
+            Move forward by one element in the history.
+            Raises an IndexError if already at the end of the history.
+            Returns:
+            Item in the history after moving forward one step.
+            """
             if self._histPtr == len(self._history):
                 raise IndexError("Tries to got beyond end of history.")
             
@@ -29,21 +53,24 @@ class Terminal:
             else:
                 return self._history[self._histPtr]
 
-        # sets ptr to end
         def append(self, item):
+            """Append a new item to the history and move the history pointer to the end."""
             if len(self._history) == 0 or item != self._history[-1]:
                 self._history.append(item)
                 self._histPtr = len(self._history)
+            # overflow length => crop list
             if len(self._history) > self._maxLength:
                 self._history = self._history[1:]
                 self._histPtr -= 1
-            
+
         def reset(self):
+            """Reset all changes done by History.back() and History.forward()."""
             self._current = ""
             self._histPtr = len(self._history)
 
 
         def dump(self, showNumbers=True):
+            """Return a string representation of the history."""
             if showNumbers:
                 return "\r\n".join("{:5d}  {:s}".format(i, self._history[i])
                                for i in range(len(self._history)))
@@ -57,28 +84,34 @@ class Terminal:
         self.prompt = "$ "
 
     def activate(self):
+        """Prime the terminal for input. Returns a context manager for the terminal."""
         class TermMngr:
             def __enter__(self):
                 pass
-
             def __exit__(self, exc_type, exc_value, traceback):
                 return False
             
         return TermMngr()
     
     def get_input(self):
+        """
+        Query user for input.
+        Return:
+        The string entered. If the user entered EOF, 'exit' is returned.
+        """
         inp = None
         while not inp:
             try:
                 inp = input(self.prompt)
-            except EOFError:
+            except EOFError:  # entered EOF (ctrl+d)
                 self.print("exit", end="")
                 inp = "exit"
-            except KeyboardInterrupt:
+            except KeyboardInterrupt:  # pressed ctrl+c
                 self.print()
         self.history.append(inp)
         return inp
 
     def print(self, *args, **kwargs):
+        """Print something to the terminal."""
         print(*args, **kwargs)
     
--- vt100.py
+++ vt100.py
@@ -6,6 +6,11 @@ from ascii_codes import ASCII
 from terminal import Terminal
 
 class VT100(Terminal):
+    """
+    Manager for VT100 emulators.
+    Supports more advanced features than the fallback but must be activated bafore use.
+    """
+    
     def __init__(self):
         super(VT100, self).__init__()
 
@@ -13,8 +18,11 @@ class VT100(Terminal):
         self._rawMode = False
         self._inStr = ""
         self._cursor = 0 # relative to inStr, not counting prompt
+        
+        # function to handle a single character of input
         self._handle_input = self._default_input_handle
 
+        # mapps special input character codes to functions that do soemthing appropriately
         self._do = {
             ASCII.ETX: self._do_abort,
             ASCII.EOT: self._do_exit,
@@ -27,9 +35,11 @@ class VT100(Terminal):
             ASCII.ESC: self._do_escape_sequence,
             ASCII.DEL: self._do_delete_backwards
         }
+        # ignore all other ASCII control codes
         for key in [0,1,2,5,6,7]+list(range(14,27))+list(range(28,32)):
             self._do[key] = self._do_ignore
 
+        # maps escape sequences to some functions
         self._do_esc = {
             "[A": self._do_up,
             "[B": self._do_down,
@@ -38,8 +48,14 @@ class VT100(Terminal):
         }
 
     def _raw_mode(self):
+        """
+        Switch terminal to raw mode.
+        Attention:
+        Must be switched back before the program terminates!
+        See VT100._reset() and VT100.activate().
+        """
+        
         self._oldattrs = termios.tcgetattr(self.inFD)
-
         try:
             tty.setraw(self.inFD)
             self._rawMode = True
@@ -49,14 +65,12 @@ class VT100(Terminal):
             raise
         
     def _reset(self):
+        """Reset the terminal to its previous state before calling VT100._raw_mode()."""
         termios.tcsetattr(self.inFD, termios.TCSADRAIN, self._oldattrs)
         self._rawMode = False
-
-    # does not touch _inStr
-    def _clear_output_from_cursor(self):
-        self.print(chr(ASCII.ESC)+"[K", end="")
-            
+    
     def _move_cursor_left(self, amt=1):
+        """Shift cursor left by amt."""
         if amt == 0:
             pass
         elif self._cursor >= amt:
@@ -69,6 +83,7 @@ class VT100(Terminal):
                 self.print((chr(ASCII.ESC)+"[D")*-amt, end="")
 
     def _move_cursor_right(self, amt=1):
+        """Shift cursor right by amt."""
         if amt == 0:
             pass
         elif self._cursor <= len(self._inStr)-amt:
@@ -80,7 +95,12 @@ class VT100(Terminal):
                 # self.print(chr(ASCII.ESC)+"[{}D".format(amt), end="")
                 self.print((chr(ASCII.ESC)+"[D")*amt, end="")
 
+    def _clear_output_from_cursor(self):
+        """Erase all printed output to the right of cursor. Does not change _inStr"""
+        self.print(chr(ASCII.ESC)+"[K", end="")
+
     def _clear_input(self):
+        """Erase all input both in _inStr and in the terminal."""
         if self._inStr:
             self._inStr = ""
             if self._cursor > 0:
@@ -88,32 +108,35 @@ class VT100(Terminal):
             self._clear_output_from_cursor()
                 
     def _do_up(self):
+        """Handle 'cursor up' command. Navigates history."""
         try:
             aux = self.history.back(self._inStr)
         except IndexError:
             return
-        
         self._clear_input()
         if aux:
             self._insert(aux)
 
     def _do_down(self):
+        """Handle 'cursor down' command. Navigates history."""
         try:
             aux = self.history.forward()
         except IndexError:
             return
-
         self._clear_input()
         if aux:
             self._insert(aux)
 
     def _do_right(self):
+        """Handle 'cursor right' command. Simply shifts cursor."""
         self._move_cursor_right()
 
     def _do_left(self):
+        """Handle 'cursor left' command. Simply shifts cursor."""
         self._move_cursor_left()
-            
+    
     def _do_abort(self):
+        """Abort and clear current input; reprint prompt."""
         self.print("^C")
         self._inStr = ""
         self._cursor = 0
@@ -121,29 +144,34 @@ class VT100(Terminal):
         self.print(self.prompt, end="")
 
     def _do_exit(self):
+        """Returns 'exit' if input is empty."""
         if not self._inStr:
             self.print("exit")
             return "exit"
             
     def _do_delete_backwards(self):
+        """Remove one character before the cursor."""
         if self._cursor > 0:
             left = self._inStr[:self._cursor-1]
             right = self._inStr[self._cursor:]
             self._inStr = left+right
 
-            # move cursor left one, remove everything after it
+            # move cursor left one, remove everything after it, and print right
             # this moves cursor to end of line
             self.print(chr(ASCII.ESC)+"[D"+chr(ASCII.ESC)+"[K"+right, end="")
             self._cursor += len(right)-1
             # move cursor back
             self._move_cursor_left(len(right))
 
-
     def _do_autocomplete(self):
-        # TODO
+        """TODO: implement"""
         pass
 
     def _do_enter(self):
+        """
+        Enter the input into history and return it.
+        Only reprints prompt if input is empty.
+        """
         if self._inStr:
             self.history.append(self._inStr)
             self.print()
@@ -153,18 +181,26 @@ class VT100(Terminal):
             self.print(self.prompt, end="")
 
     def _do_escape_sequence(self):
+        """Switch state to inputting escape sequence."""
         self._handle_input = self._esc_input_handle
         self._escSeq = ""
 
     def _do_ignore(self):
+        """Don't do nothing."""
         pass
 
     def _insert(self, s):
+        """Insert a string into the input; adjust cursor"""
         self._inStr += s
         self._cursor += len(s)
         self.print(s, end="")
     
     def _default_input_handle(self, c):
+        """
+        Handle input of single characters via VT100._do.
+        Returns:
+        Result of function associated with c.
+        """
         try:
             return self._do[ord(c)]()
         except KeyError:
@@ -172,20 +208,48 @@ class VT100(Terminal):
             # no return -> definately not done yet
         
     def _esc_input_handle(self, c):
+        """
+        Handle input of escape sequences.
+        Collects characters for sequence and execute it once complete.
+        Switches back to _default_input_handle when done.
+        Returns:
+        Result of executing function for escape sequence.
+        """
         self._escSeq += c
         oc = ord(c)
 
-        # TODO check these numbers!
-        if c != "[" and oc >= 64 and oc <= 95:
+        if c != "[" and oc >= 64 and oc <= 126:
             self._handle_input = self._default_input_handle
             try:
                 return self._do_esc[self._escSeq]()
             except KeyError:
                 self._insert("^"+self._escSeq)
+
+    def activate(self):
+        """
+        Prime the terminal for input.
+        Switches to raw mode and returns a context manager to switch back
+        when the shell is terminated.
+        """
+        class TermMngr:
+            def __init__(self, term):
+                self.term = term
+            def __enter__(self):
+                pass
+            def __exit__(self, exc_type, exc_value, traceback):
+                self.term._reset()
+                return False
+        
+        self._raw_mode()
+        return TermMngr(self)
             
     def get_input(self):
+        """
+        Query user for input.
+        Return:
+        The string entered. If the user entered EOF, 'exit' is returned.
+        """
         self.print(self.prompt, end="")
-        
         self._inStr = ""
         self._cursor = 0
         while True:
@@ -195,6 +259,10 @@ class VT100(Terminal):
 
 
     def print(self, *args, end="\n", **kwargs):
+        """
+        Print something to the terminal.
+        The built in print function does not work properly when in raw mode.
+        """
         if self._rawMode:
             if end:
                 # need explicit carriage return
@@ -205,18 +273,4 @@ class VT100(Terminal):
             print(*args, end=end, **kwargs)
             
         sys.stdout.flush()
-
-    def activate(self):
-        class TermMngr:
-            def __init__(self, term):
-                self.term = term
-
-            def __enter__(self):
-                pass
-
-            def __exit__(self, exc_type, exc_value, traceback):
-                self.term._reset()
-                return False
         
-        self._raw_mode()
-        return TermMngr(self)

1e0a48592848b5e224d175535734638b831d5c3d
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2017-07-07 13:55:50 +0200

    Combine h5shell.py and shell.py

--- h5sh.py
+++ h5sh.py
@@ -1,21 +1,46 @@
+"""
+Contains the shell and serves as main program to execute.
+"""
+
+import argparse
 import shlex
 import os.path
 
 from commands import *
 from h5manager import H5Manager
 
-class Shell:
+# import best available terminal backend
+try:
+    from vt100 import VT100 as Term
+except ImportError:
+    from terminal import Terminal as Term
+
+
+def parse_args():
+    """Parse command line arguments for h5sh."""
+    parser = argparse.ArgumentParser(description="Interactive shell for HDF5 files.")
+    parser.add_argument("FILE", help="HDF5 file to open")
+    return parser.parse_args()
+
+
+class H5sh:
+    """
+    The actual shell which glues all pieces together.
+    """
+
     def __init__(self, term):
         self._term = term
         self._fname = ""
         self._wd = []
 
+        # dict of available commands
         self._cmds = {
             "ls": ls.ls(),
             "cd": cd.cd(),
             "pwd": pwd.pwd()
         }
 
+        # dict of aliases (evaluated before _cmds)
         self._aliases = {
             "..": "cd ..",
             "l": "ls -l"
@@ -42,6 +67,10 @@ class Shell:
         return prompt
 
     def run(self, fname):
+        """
+        Main REPL to run the shell.
+        """
+
         self._fname = fname
         self._wd = []
 
@@ -67,3 +96,7 @@ class Shell:
             except KeyError:
                 self._term.print("h5sh: {}: command not found".format(inp[0]))
 
+
+if __name__ == "__main__":
+    # run the shells REPL
+    H5sh(Term()).run(parse_args().FILE)

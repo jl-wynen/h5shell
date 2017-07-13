edcf3b606536ee08d5e0bdf90b2b5437a0529e53
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2017-07-13 09:56:00 +0200

    Extract version from git tag

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import os.path
+import subprocess
 
 def readme():
     with open("README.rst", "r") as f:
         return f.read()
 
+def get_version():
+    try:
+        return subprocess.check_output(["git", "describe"]).rstrip().decode("utf-8")
+    except subprocess.CalledProcessError:
+        return "0.0"  # no tag set, default to 0.0
+
 setup(
     name="h5sh",
-    version="0.1",
+    version="{}".format(get_version()),
     description="An interactive shell for HDF5 files",
     long_description=readme(),
     keywords="h5sh hdf5 h5 shell",
